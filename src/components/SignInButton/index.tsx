1c64c0438793ccff417c64f997d16c13636af0b8
Marcelo Rossignoli <mattosrossignoli@gmail.com>
2022-07-10 20:26:03 -0300

    add fauna db

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,8 +2,12 @@ import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 ;import { signIn,signOut, useSession  } from "next-auth/react"
+
+
+
 export function SignInButton() {
   const { data: session, status }= useSession()
+  
   return status  === "authenticated" ? (
     <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import {fauna} from '../../../services/fauna'
+import {query as q} from 'faunadb'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -9,4 +11,22 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn(user: any,account: any,profile: any): Promise<any>{
+      const {email} = user.user
+
+      try{
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {
+            data: {email}    
+          }
+        )
+      )
+      return true
+    }catch(e){
+      return false
+    }
+    }}
 })
